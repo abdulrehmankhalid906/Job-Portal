47b796f59d9271c34f112d5ef72316b73bffb994
Abdul Rehman Khalid <arehman@tkbench.com>
2024-10-19 10:51:08 +0500

    Major Bug Fixed

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -23,7 +23,7 @@
 
                                 <div class="col-auto">
                                     <div class="stat text-primary">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-dollar-sign align-middle"><line x1="12" y1="1" x2="12" y2="23"></line><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path></svg>
+                                        <i class="align-middle" data-feather="slack"></i> 
                                     </div>
                                 </div>
                             </div>
@@ -45,7 +45,7 @@
 
                                 <div class="col-auto">
                                     <div class="stat text-primary">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-bag align-middle"><path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path><line x1="3" y1="6" x2="21" y2="6"></line><path d="M16 10a4 4 0 0 1-8 0"></path></svg>
+                                        <i class="align-middle" data-feather="list"></i> 
                                     </div>
                                 </div>
                             </div>
@@ -67,7 +67,7 @@
 
                                 <div class="col-auto">
                                     <div class="stat text-primary">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity align-middle"><polyline points="22 12 18 12 15 21 9 3 6 12 2 12"></polyline></svg>
+                                        <i class="align-middle" data-feather="users"></i> 
                                     </div>
                                 </div>
                             </div>
--- resources/views/package/update_package.blade.php
+++ resources/views/package/update_package.blade.php
@@ -1,11 +1,14 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="row">
-    <div class="col-lg-12">
-        <div class="card-header align-items-center d-flex">
-            <h4 class="card-title mb-0 flex-grow-1">Edit Package</h4>
-            <div class="flex-shrink-0">
+<main class="content">
+    <div class="container-fluid p-0">
+        <div class="row mb-xl-0">
+            <div class="col-auto d-none d-sm-block">
+                <h3><strong>Edit</strong> Package</h3>
+            </div>
+
+            <div class="col-auto ms-auto text-end mt-n1">
                 <a href="{{ route('packages.index') }}" class="btn btn-success btn-label btn-sm">
                     <i class="ri-add-fill label-icon align-middle fs-16 me-2"></i> All Packages
                 </a>
@@ -82,7 +85,7 @@
             </div>
         </div>
     </div>
-</div>
+</main>
 @endsection
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
