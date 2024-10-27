7c94814106c14f44c5404d4a90c5d31bebcf0059
Abdul Rehman Khalid <arehman@tkbench.com>
2024-10-27 21:32:31 +0500

    Chat Module

--- app/Models/User.php
+++ app/Models/User.php
@@ -71,4 +71,9 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasMany(Notification::class);
     }
+    
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
 }
